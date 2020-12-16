9f513073414196068efc466fb6a0deee53fd4eff
Sean Lynch <sean@webdoc.digital>
2020-12-16 17:06:42 +0000

    Implement endsWith assertion

---
+++ src/functions/endsWith/index.test.ts
@@ -0,0 +1,22 @@
+import AssertionFailedException from '../../Exceptions/AssertionFailedException';
+import endsWith from './';
+
+describe('endsWith', () => {
+    it ('should return true if the value ends with the needle', () => {
+        expect(endsWith('Sean Lynch')('Lynch')).toBeTruthy();
+    });
+
+    it ('should return true if the value starts and ends with the needle', () => {
+        expect(endsWith('Socks')('Socks')).toBeTruthy();
+    });
+
+    it ('should throw an error if the value doesnt end with the needle', () => {
+        expect(() => {
+            endsWith('Too old, to fucking blind')('john daniels');
+        }).toThrow(AssertionFailedException);
+
+        expect(() => {
+            endsWith('Too old, to fucking blind')('Too old');
+        }).toThrow(AssertionFailedException);
+    });
+});
