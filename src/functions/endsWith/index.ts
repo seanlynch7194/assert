baf6841fcd95d52ddea7fbbfcc5dc4da368b4eaf
Sean Lynch <sean@webdoc.digital>
2020-12-16 17:19:00 +0000

    Add inline documentation for endsWith

--- src/functions/endsWith/index.ts
+++ src/functions/endsWith/index.ts
@@ -1,5 +1,10 @@
 import AssertionFailedException from '../../Exceptions/AssertionFailedException';
 
+/**
+ * Assert that string ends with a sequence of chars.
+ * 
+ * @param value 
+ */
 const endsWith = (value: string) => (needle: string): boolean => {
     if (!value.endsWith(needle)) {
         throw new AssertionFailedException(`"${value}" doesn't end with "${needle}"`);
